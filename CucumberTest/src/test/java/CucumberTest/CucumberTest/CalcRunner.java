5ffa5017146dae9fe9886a5979b12ff4c1bb968f
snehasishdu <31220383+snehasishdu@users.noreply.github.com>
2018-01-16 10:26:57 +0530

    Invalid Changes

--- CucumberTest/src/test/java/CucumberTest/CucumberTest/CalcRunner.java
+++ CucumberTest/src/test/java/CucumberTest/CucumberTest/CalcRunner.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 @RunWith(Cucumber.class)
-//Runner Options to Run this Test After Conflict Resolve
-@CucumberOptions(features= {"E:\\Maven-Selenium-Cucumber\\CucumberTest\\target\\test-classes\\CucumberTest\\CucumberTest\\Calc.feature"},plugin = {"html:target/cucumber-html-report"},tags = {"@Smoke"})
+//Runner Options to Run this Test After Conflict Resolve123
+@CucumberOptions(features= {"E:\\Maven-Selenium-Cucumber\\CucumberTest\\target\\test-classes\\CucumberTest\\CucumberTest\\Calc.feature"},plugin = {"html:target/cucumber-html-report"},tags = {"@Smoke123"})
 public class CalcRunner extends AbstractTestNGCucumberTests{ }
